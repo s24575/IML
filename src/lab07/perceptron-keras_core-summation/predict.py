b7c20903d49c1b0c8f42cbc965ca2123886425d0
s24575 <sawickigrzegorz66@gmail.com>
2023-11-27 11:46:35 +0100

    lab07 first commit

---
+++ src/lab07/perceptron-keras_core-summation/predict.py
@@ -0,0 +1,27 @@
+﻿# Predictions, using previously trained neural network
+
+# Set TensorFlow backend
+import os
+os.environ["KERAS_BACKEND"] = "tensorflow"
+
+from keras_core.models import load_model
+import numpy
+
+# Load test dataset
+dataset = numpy.loadtxt("test_data.csv", delimiter=",")
+
+# Input vector size
+NIN = 2
+
+# Split into input (X) and output (Y) variables
+X = dataset[:, 0:NIN]
+Y = dataset[:, NIN:]
+
+model = load_model("summation.keras")
+
+predictions = model.predict(X)
+
+print("\nTest results:")
+# Caution: the following loop makes sense only for summation of 2 numbers
+for i in range(len(Y)):
+    print(f"{X[i][0]} + {X[i][1]} = {predictions[i][0]:.4f} (expected: {Y[i][0]})")
---
+++ src/lab07/perceptron-keras_core-summation/train.py
@@ -0,0 +1,53 @@
+﻿# Training of the neural network using Keras Core
+
+# Set TensorFlow backend
+import os
+os.environ["KERAS_BACKEND"] = "tensorflow"
+
+from keras_core.models import Sequential
+from keras_core.layers import Input
+from keras_core.layers import Dense
+import numpy
+
+# No of epochs for training
+EPOCHS = 1000
+
+# Input vector size
+NIN = 2
+
+# Output vector size
+NOUT = 1
+
+# Load dataset
+dataset = numpy.loadtxt("train_data.csv", delimiter=",")
+
+# Split into input (X) and output (Y) variables
+# Data format: x1,x2,...,xNIN,y1,y2,...,yNOUT
+#              x2,y2,...,zNIN,y1,y2,...,yNOUT
+
+# Create model
+# https://keras.io/layers/core/
+# https://keras.io/activations/
+X = dataset[:, 0:NIN]
+Y = dataset[:, NIN:]
+
+model = Sequential()
+# Input layer (NIN inputs)
+model.add(Input(shape=(NIN,)))
+# Simple perceptron (when NOUT = 1) or single layer (when NOUT > 1)
+model.add(Dense(NOUT, activation="tanh"))
+# Example multi-layer network (NIN - 3 - 12 - NOUT neurons):
+#model.add(Dense(3, input_dim=NIN, activation="tanh"))
+#model.add(Dense(12, input_dim=NIN, activation="tanh"))
+#model.add(Dense(NOUT, activation="tanh"))
+
+# Compile model
+# Loss functions: https://keras.io/losses/
+# Optimizers: https://keras.io/optimizers/
+model.compile(loss="mean_squared_error", optimizer="sgd")
+
+# Train the model
+model.fit(X, Y, epochs=EPOCHS, batch_size=1)
+
+# Save model to a file
+model.save("summation.keras")

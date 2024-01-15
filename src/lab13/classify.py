ab62b3127b2fc6f4bfade06d03d94c50a52c35e8
s24575 <sawickigrzegorz66@gmail.com>
2024-01-15 11:39:52 +0100

    lab13

---
+++ src/lab13/classify.py
@@ -0,0 +1,91 @@
+﻿import json
+import os
+from collections import Counter
+
+# Disable TF warning messages and set backend
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ["KERAS_BACKEND"] = "tensorflow"
+
+import keras_core as keras
+import numpy as np
+import cv2     # python -m pip install opencv-python
+
+from keras_core.datasets import mnist
+
+# Directory with test set
+TEST_DATASET_DIR = 'mnist-test'
+
+# Trained model filename
+MODEL = 'model.keras'
+
+if __name__ == "__main__":
+    
+    # Load trained model
+    model = keras.models.load_model(MODEL)
+
+    (_, _), (x_test, y_test) = mnist.load_data()
+
+    NO_OF_CLASSES = 10
+    y_test_categorical = keras.utils.to_categorical(y_test, NO_OF_CLASSES)
+
+    evaluation = model.evaluate(x_test, y_test_categorical)
+
+    print(f'Accuracy: {evaluation[1]}')
+    print(f'Precision: {np.mean(evaluation[2])}')
+    print(f'Recall: {evaluation[3]}')
+    print(f'F1 Score: {evaluation[4]}')
+
+    for image, true_label in zip(x_test, y_test):
+        # Pre-process the image for classification
+        image_data = image.astype('float32') / 255
+        image_data = keras.preprocessing.image.img_to_array(image_data)
+        # Expand dimension (28,28,1) -> (1,28,28,1)
+        image_data = np.expand_dims(image_data, axis=0)
+        
+        # Classify the input image
+        prediction = model.predict(image_data, verbose=0)
+        pred_flat = prediction.flatten()
+        
+        n = 3
+        sorted_indices = np.argsort(pred_flat)[::-1]
+        top_n_indices = sorted_indices[:n]
+        top_n_probabilities = pred_flat[top_n_indices]
+
+        label_list = []
+        for index, prob in zip(top_n_indices, top_n_probabilities):
+            prob_pct = prob * 100
+            if prob_pct < 1:
+                break
+            label_list.append("{}: {:0.2f}%".format(index, prob_pct))
+        label_pred = ", ".join(label_list)
+
+        label_actual = f"actual: {true_label}"
+
+        # Draw the label on the image
+        output_image = cv2.resize(image, (500, 500))
+        cv2.putText(output_image, label_pred, (10, 25),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
+        cv2.putText(output_image, label_actual, (10, 55),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
+
+        # Show the output image        
+        cv2.imshow("Output", output_image)
+        
+        # Break on 'q' pressed, continue on the other key
+        if cv2.waitKey(0) & 0xFF == ord('q'):
+            break
+
+    misclassifications = Counter()
+
+    predictions = model.predict(x_test)
+    for i in range(len(predictions)):
+        true_label = int(y_test[i])
+        predicted_label = int(predictions[i].argmax())
+        if true_label != predicted_label:
+            misclassification_key = f"{true_label},{predicted_label}"
+            misclassifications[misclassification_key] += 1
+
+    misclassifications_sorted = dict(sorted(misclassifications.items(), key=lambda item: item[1], reverse=True))
+
+    with open("mnist_mislabellings.json", "w") as json_file:
+        json.dump(misclassifications_sorted, json_file, indent=2)
